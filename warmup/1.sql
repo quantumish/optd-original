5218ae7cb0d80e79f19185d581c4c636db50645d
Gun9niR <gun9nir.guo@gmail.com>
2024-04-30 20:02:35 -0400

    update warmup

--- warmup/1.sql
+++ warmup/1.sql
@@ -1,6 +1,5 @@
 SELECT *
 FROM movie_companies mc,
      movie_info_idx mi_idx
-WHERE 
-    AND mi_idx.info_type_id=112
-    AND mc.company_type_id=2;
+WHERE mi_idx.info_type_id=112
+      AND mc.company_type_id=2;
