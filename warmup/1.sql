77f542031d7e9fa1f91aacdec0b31cdc5e84131e
Gun9niR <gun9nir.guo@gmail.com>
2024-04-30 21:10:43 -0400

    update warmup for query 3a

--- warmup/1.sql
+++ warmup/1.sql
@@ -1,4 +1,5 @@
 SELECT *
-FROM movie_companies mc,
+FROM movie_keyword mk,
     title t
-WHERE t.id=mc.movie_id;
+WHERE t.id=mc.movie_id
+AND t.production_year>2005;
--- warmup/2.sql
+++ warmup/2.sql
@@ -1,5 +1,5 @@
 SELECT *
 FROM title t,
-    movie_keyword mk
-WHERE t.id=mk.movie_id 
-    AND mk.keyword_id=117;
+    movie_info mi
+WHERE t.id=mi.movie_id 
+   AND t.production_year>2005;
