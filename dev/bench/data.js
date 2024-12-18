2a7de37460911fd5368a029714836ef02cbdde97
github-action-benchmark <github@users.noreply.github.com>
2024-12-18 16:46:38 +0000

    add Rust Benchmark (cargo) benchmark result for 4ed7833a34e1a9f518d066c6b641840681da1ab1

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1734540398537,
+  "repoUrl": "https://github.com/cmu-db/optd",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "connor.tsui20@gmail.com",
+            "name": "Connor Tsui",
+            "username": "connortsui20"
+          },
+          "committer": {
+            "email": "connor.tsui20@gmail.com",
+            "name": "Connor Tsui",
+            "username": "connortsui20"
+          },
+          "distinct": true,
+          "id": "4ed7833a34e1a9f518d066c6b641840681da1ab1",
+          "message": "add tpch benchmark workflow",
+          "timestamp": "2024-12-18T11:44:40-05:00",
+          "tree_id": "15bcaa1052f80c94b538359e74036d94fd7934aa",
+          "url": "https://github.com/cmu-db/optd/commit/4ed7833a34e1a9f518d066c6b641840681da1ab1"
+        },
+        "date": 1734540398266,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tpch/q6/planning",
+            "value": 341620,
+            "range": "± 16439",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tpch/q6/execution",
+            "value": 149867,
+            "range": "± 6894",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
