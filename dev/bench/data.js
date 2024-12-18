c64d913eb5229a6ee779daea1031483be923910f
Connor Tsui <connor.tsui20@gmail.com>
2024-12-18 12:11:08 -0500

    remove q6 extra bench

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,42 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "connortsui20"
           },
           "distinct": true,
-          "id": "4ed7833a34e1a9f518d066c6b641840681da1ab1",
-          "message": "add tpch benchmark workflow",
-          "timestamp": "2024-12-18T11:44:40-05:00",
-          "tree_id": "15bcaa1052f80c94b538359e74036d94fd7934aa",
-          "url": "https://github.com/cmu-db/optd/commit/4ed7833a34e1a9f518d066c6b641840681da1ab1"
-        },
-        "date": 1734540398266,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tpch/q6/planning",
-            "value": 341620,
-            "range": "± 16439",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "tpch/q6/execution",
-            "value": 149867,
-            "range": "± 6894",
-            "unit": "ns/iter"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "connor.tsui20@gmail.com",
-            "name": "Connor Tsui",
-            "username": "connortsui20"
-          },
-          "committer": {
-            "email": "connor.tsui20@gmail.com",
-            "name": "Connor Tsui",
-            "username": "connortsui20"
-          },
-          "distinct": true,
           "id": "9d8ee74558550c65e1a80802329cb1f09218454f",
           "message": "add tpch benchmark workflow",
           "timestamp": "2024-12-18T11:53:00-05:00",
